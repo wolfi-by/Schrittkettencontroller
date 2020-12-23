6df63c372ae6d40330d7224d8572a66efcb12d94
wolfi-by <w.hoehne@outlook.de>
2020-12-23 09:21:38 +0100

    Projektdateien hinzufügen.

---
+++ Data/TextHandler.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Timers;
+using S7.Net;
+
+namespace Schrittkettencontroller.Data
+{
+    public class TextHandler
+    {
+        public bool isEnabled
+        {
+            get
+            {
+                return timer != null ? timer.Enabled : false;
+            }
+        }
+
+        public List<Textelement> elements { get; set; } = new List<Textelement>();
+        private Timer timer;
+        public Textelement current { set; get; } = new Textelement();
+
+
+
+
+
+        private void TickHandler(object sender, ElapsedEventArgs e)
+        {
+            timer.Stop();
+            var index = elements.IndexOf(current);
+            current = index < elements.Count - 1 ? current = elements[index + 1] : null;
+            if (current != null)
+            {
+                StartTimer();
+            }
+            else
+            {
+                current = new Textelement();
+                timer.Dispose();
+                timer = null;
+            }
+            NotifyDataChanged();
+        }
+
+        public void Start()
+        {
+            timer = new Timer();
+            timer.Elapsed += TickHandler;
+            timer.AutoReset = false;
+            current = elements.FirstOrDefault();
+            StartTimer();
+        }
+
+        public void Stop()
+        {
+            timer.Stop();
+            timer.Dispose();
+            timer = null;
+            current = new Textelement();
+            NotifyDataChanged();
+        }
+
+        private void StartTimer()
+        {
+
+
+            if (!string.IsNullOrEmpty(current.AdressToWrite))
+            {
+                var p = new S7.Net.Plc(CpuType.S7300, "192.168.178.210", 0, 2);
+                try
+                {
+                    p.Open();
+
+                    p.Write(current.AdressToWrite.ToUpper(), current.ValueToWrite);
+                    Console.WriteLine($"Written data: {p.Read(current.AdressToWrite)}");
+                }
+
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    p.Close();
+                }
+
+
+            }
+            timer.Interval = current.GetInterval.TotalMilliseconds;
+            timer.Start();
+        }
+
+        public void AddItem(Textelement item)
+        {
+            item.ID = FindFreeNumber();
+
+            if (item.GetInterval == TimeSpan.Zero) item.Interval = 1;
+            elements.Add(item);
+            NotifyDataChanged();
+        }
+
+        public void RemoveItem(Textelement item)
+        {
+            elements.Remove(item);
+            NotifyDataChanged();
+        }
+
+        public event Action OnChange;
+
+        private void NotifyDataChanged() => OnChange?.Invoke();
+
+        private int FindFreeNumber()
+        {
+            if (elements.Count > 0)
+            {
+                var max = elements.Max(t => t.ID);
+                return max + 1;
+            }
+            return 0;
+        }
+
+        public string GetText()
+        {
+            string Text = string.Empty;
+            foreach (var item in elements)
+            {
+                Text += item.Text + "<br />";
+            }
+            return Text;
+        }
+    }
+}
---
+++ Data/Textelement.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Schrittkettencontroller.Data
+{
+
+    public class Textelement
+    {
+        public string Text { set; get; }
+        public int ID { set; get; }
+        private TimeSpan _Interval;
+        public TimeSpan GetInterval
+        {
+            get
+            {
+                return _Interval;
+            }
+        }
+        public int Interval
+        {
+            get
+            {
+                return (int)_Interval.TotalSeconds;
+            }
+            set
+            {
+                _Interval = TimeSpan.FromSeconds(value);
+            }
+        }
+        public string AdressToWrite { set; get; }
+        public string ValueToWrite { set; get; }
+
+
+    }
+}
